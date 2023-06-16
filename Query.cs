e7e0bcccc6a2137565d127c4f8173ea6ca594e2e
Jan Hannes Petersen <petersen@willems-daten.de>
2023-06-16 10:23:18 +0200

    Projektdateien hinzufügen.

---
+++ Query.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace IssueNullViolation
+{
+    public class Query
+    {
+        public Person GetPerson() => new("Luke Skywalker")
+        {
+            Children = new List<Person>()
+            {
+                new("Anakin Skywalker"),
+                // why does HC 13.2.1 null out children field when a child is null
+                // but the response doesn't contain an error property??!
+                null
+            }
+        };
+    }
+
+    public class Person
+    {
+        public Person(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; }
+
+        public ICollection<Person>? Children { get; set; }
+    }
+}
